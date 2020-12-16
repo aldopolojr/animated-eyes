9909cb333855d59c3f183ccc1b607f7e2ea1aac4
aldopolojr <aldopolojr@icloud.com>
2020-12-16 18:01:23 +0100

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello world"); 
+document.querySelector('body').addEventListener('mousemove', eyeball);
+
+function eyeball() {
+    const eyes = document.querySelectorAll('.eye');
+
+    eyes.forEach(eye => {
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rotation = (radian * (180 / Math.PI) * -1) + 270;
+        eye.style.transform = `rotate(${rotation}deg)`;
+    });
+}
